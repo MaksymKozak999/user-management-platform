2341e221df9fc24afc59316d9a0466ef22e39ba8
Maksym <arabovicarab589@email.com>
2026-08-09 17:55:38 +0200

    feat: add Flyway database migrations

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -13,14 +13,11 @@
     @RequestMapping("/users")
     public class UserController {
 
-
         private final UserService userService;
-
 
         public UserController(UserService userService) {
             this.userService = userService;
         }
-
 
         @GetMapping
         public List<UserResponse> getUsers() {
@@ -39,7 +36,6 @@
             return userService.addUser(user);
         }
 
-
         @PutMapping("/{id}")
         public UserResponse updateUser(
                 @PathVariable Long id,
@@ -47,7 +43,6 @@
 
             return userService.updateUser(id,user);
         }
-
 
         @DeleteMapping("/{id}")
         public void deleteUser(@PathVariable Long id) {
